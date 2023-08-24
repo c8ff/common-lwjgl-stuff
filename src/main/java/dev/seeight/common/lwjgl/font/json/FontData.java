a45f63e54fccb6b7dc1ddcf394bb54d310e5875d
C8FF <theprestigefox@googlemail.com>
2023-08-23 18:10:46 -0600

    Add additional properties to FontData

--- src/main/java/dev/seeight/common/lwjgl/font/json/FontData.java
+++ src/main/java/dev/seeight/common/lwjgl/font/json/FontData.java
@@ -3,6 +3,8 @@ package dev.seeight.common.lwjgl.font.json;
 import java.util.Map;
 
 public class FontData {
+	public String name;
+	public int size;
 	public int width;
 	public int height;
 	public Map<String, CharacterData> characters;
